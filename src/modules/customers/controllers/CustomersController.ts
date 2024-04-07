9003e30b857722b129adea4a69f71134afb7cb36
Eryálef Vieira <eryalef@gmail.com>
2024-04-06 23:27:26 -0300

    Customer controller

---
+++ src/modules/customers/controllers/CustomersController.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+import ListCustomerService from "../services/ListCustomerService";
+import ShowCustomerService from "../services/ShowCustomerService";
+import CreateCustomerService from "../services/CreateCustomerService";
+import UpdateCustomerService from "../services/UpdateCustomerService";
+import DeleteCustomerService from "../services/DeleteCustomerService";
+
+export default class CustomersController {
+  public async index(request: Request, response: Response) {
+    const listCustomer = new ListCustomerService();
+
+    const customers = await listCustomer.execute();
+
+    response.json(customers);
+  }
+  public async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const showCustomer = new ShowCustomerService();
+
+    const customer = await showCustomer.execute({id});
+
+    response.json(customer);
+  }
+  public async create(request: Request, response: Response) {
+    const { name, email } = request.body;
+
+    const createCustomer = new CreateCustomerService();
+
+    const customer = await createCustomer.execute({ name, email });
+
+    response.json(customer);
+  }
+  public async update(request: Request, response: Response) {
+    const { id } = request.params;
+    const { name, email } = request.body;
+
+    const updateCustomer = new UpdateCustomerService();
+
+    const customer = await updateCustomer.execute({ id, name, email });
+
+    response.json(customer);
+  }
+  public async delete(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const deleteCustomer = new DeleteCustomerService();
+
+    await deleteCustomer.execute({ id });
+
+    response.json([])
+  }
+}
