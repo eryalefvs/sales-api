36d0a3bb59dcb5ec34a650356321ea98d59d2b11
Eryálef Vieira <eryalef@gmail.com>
2024-04-06 23:28:08 -0300

    Customer route

---
+++ src/modules/customers/routes/customer.route.ts
@@ -0,0 +1,53 @@
+import { Router } from "express"
+import CustomersController from "../controllers/CustomersController";
+import { celebrate, Joi, Segments } from "celebrate"
+import isAuthenticated from "@shared/http/middlewares/isAuthenticated";
+
+const customersRouter = Router();
+const customerController = new CustomersController();
+
+customersRouter.use(isAuthenticated);
+
+customersRouter.get("/", customerController.index);
+customersRouter.get("/:id",
+celebrate({
+  [Segments.PARAMS]: {
+    id: Joi.string().uuid().required(),
+  }
+}),
+customerController.show,
+);
+
+customersRouter.post("/",
+celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+  }
+}),
+customerController.create,
+);
+
+customersRouter.put("/:id",
+celebrate({
+  [Segments.PARAMS]: {
+    id: Joi.string().uuid().required(),
+  },
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+  }
+}),
+customerController.update,
+);
+
+customersRouter.delete("/:id",
+celebrate({
+  [Segments.PARAMS]: {
+    id: Joi.string().uuid().required(),
+  },
+}),
+customerController.delete,
+);
+
+export default customersRouter;
