85f3be222531143f34b5cc50f5c99cce3e833cbb
Eryálef Vieira <eryalef@gmail.com>
2024-04-11 16:30:27 -0300

    Add function findAllByIds

--- src/modules/products/typeorm/repositories/ProductRepository.ts
+++ src/modules/products/typeorm/repositories/ProductRepository.ts
@@ -1,5 +1,10 @@
 import { dataSource } from "@shared/typeorm/index";
 import Product from "../entities/Product";
+import { In } from "typeorm";
+
+interface IFindProducts {
+  id: string;
+}
 
 export const ProductRepository = dataSource.getRepository(Product).extend({
   findByName(name: string) {
@@ -7,4 +12,14 @@ export const ProductRepository = dataSource.getRepository(Product).extend({
       .where("products.name = :name", { name })
       .getOne();
   },
+
+  async findAllByIds(product: IFindProducts[]) {
+    const productsIds = product.map(product => product.id);
+
+    return await this.find({
+      where: {
+        id: In(productsIds),
+      }
+    })
+  }
 });
