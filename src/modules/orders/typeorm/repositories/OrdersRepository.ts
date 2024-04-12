f742d5d99c877a0b741270d69fa7210b5ee267bf
Eryálef Vieira <eryalef@gmail.com>
2024-04-11 23:35:27 -0300

    add function findById

---
+++ src/modules/orders/typeorm/repositories/OrdersRepository.ts
@@ -0,0 +1,35 @@
+import { dataSource } from "@shared/typeorm/index";
+import Order from "../entities/Order";
+import Customers from "@modules/customers/typeorm/entities/Customers";
+
+interface IProduct {
+  product_id: string;
+  price: number;
+  quantity: number;
+}
+
+interface IRequest {
+  customer: Customers;
+  products: IProduct[];
+}
+
+export const OrdersRepository = dataSource.getRepository(Order).extend({
+  findById(id: string) {
+    return this.findOne({
+      where: {
+        id,
+      },
+      relations: ["order_products", "customer"]
+    })
+  },
+
+  async createOrder({ customer, products }: IRequest) {
+    const order = this.create({
+      customer,
+      order_products: products,
+    })
+
+    await this.save(order);
+    return order;
+  }
+});
