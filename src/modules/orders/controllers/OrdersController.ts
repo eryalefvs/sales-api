346092daf8af7193535a53017b1b803e0c09fa0a
Eryálef Vieira <eryalef@gmail.com>
2024-04-11 23:31:33 -0300

    Orders controller

---
+++ src/modules/orders/controllers/OrdersController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import ShowOrderService from "../services/ShowOrderService";
+import CreateOrderService from "../services/CreateOrderService";
+
+export default class OrdersController {
+  public async create(request: Request, response: Response){
+    const { customer_id, products } = request.body;
+
+    const createOrder = new CreateOrderService();
+
+    const order = await createOrder.execute({
+      customer_id,
+      products,
+    });
+
+    return response.json(order);
+  }
+
+  public async show(request: Request, response: Response){
+   const { id } = request.params;
+
+    const showOrder = new ShowOrderService();
+
+    const order = await showOrder.execute({ id });
+
+    return response.json(order);
+  }
+}
