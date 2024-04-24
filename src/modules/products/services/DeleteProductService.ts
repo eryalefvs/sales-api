138d9cf0d7cce6159cf611b66d9b9273bca159f2
Eryálef Vieira <eryalef@gmail.com>
2024-04-24 01:25:39 -0300

    del cache added

--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,5 +1,6 @@
 import AppError from "@shared/errors/AppError";
 import { ProductRepository } from "../typeorm/repositories/ProductRepository";
+import redisCache from "@shared/cache/RedisCache";
 
 interface IRequest {
   id: string;
@@ -15,6 +16,8 @@ class DeleteProductService {
     if(!product) {
       throw new AppError("Product not found.")
     }
+
+    await redisCache.invalidate("api-vendas-PRODUCT_LIST");
 
     await ProductRepository.remove(product)
   }
