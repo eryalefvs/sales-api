07b86914e3548b6ec6747fc454fa05ce8d182928
Eryálef Vieira <eryalef@gmail.com>
2024-04-27 22:27:21 -0300

    refactoring repository of customers

--- src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
+++ src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
@@ -1,21 +1,34 @@
-import { dataSource } from "@shared/typeorm";
+import { ICreateCustomer } from "@modules/customers/domain/models/ICreateCustomer";
 import Customers from "../entities/Customers";
+import { ICustomersRepository } from "@modules/customers/domain/repositories/ICustomersRepository";
+import { dataSource } from "@shared/infra/typeorm";
 
-export const CustomersRepository = dataSource.getRepository(Customers).extend({
-  findById(id: string) {
-    return this.createQueryBuilder("customers")
+export const CustomersRepository: ICustomersRepository = dataSource.getRepository(Customers).extend({
+  async create({name, email}: ICreateCustomer): Promise<Customers | null> {
+    const customer = this.create({name, email});
+    await this.save(customer);
+    return customer;
+  },
+
+  async save(customer: Customers): Promise<Customers | null> {
+    await this.save(customer);
+    return customer;
+  },
+
+  async findById(id: string): Promise<Customers | null> {
+    return await this.createQueryBuilder("customers")
     .where("customers.id = :id", { id })
     .getOne();
   },
 
-  findByName(name: string) {
-    return this.createQueryBuilder("customers")
+  async findByName(name: string): Promise<Customers | null> {
+    return await this.createQueryBuilder("customers")
     .where("customers.name = :name", { name })
     .getOne();
   },
 
-  findByEmail(email: string) {
-    return this.createQueryBuilder("customers")
+  async findByEmail(email: string): Promise<Customers | null> {
+    return await this.createQueryBuilder("customers")
     .where("customers.email = :email", { email })
     .getOne();
   }
